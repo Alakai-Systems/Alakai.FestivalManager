ac8844a5f462123d6b583233a01c4c3d913f7f9e
Jose Maria Farfan <magoalakai@gmail.com>
2026-06-09 17:27:17 +0200

    FestivalControler CReates Festival, and the whole performance working

---
+++ Alakai.FestivalManager.Api/Controllers/FestivalsController.cs
@@ -0,0 +1,26 @@
+﻿namespace Alakai.FestivalManager.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class FestivalsController : ControllerBase
+{
+    private readonly IFestivalService _festivalService;
+
+    public FestivalsController(IFestivalService festivalService)
+    {
+        _festivalService = festivalService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(
+        [FromBody] CreateFestivalCommand command,
+        CancellationToken cancellationToken)
+    {
+        FestivalDto result = await _festivalService.CreateAsync(command, cancellationToken);
+
+        return CreatedAtAction(
+            nameof(Create),
+            new { id = result.Id },
+            result);
+    }
+}
--- Alakai.FestivalManager.Api/Global.cs
+++ Alakai.FestivalManager.Api/Global.cs
@@ -1,2 +1,6 @@
 ﻿global using Alakai.FestivalManager.Application.Extensions;
+global using Alakai.FestivalManager.Application.Features.Festivals.Commands.CreateFestival;
+global using Alakai.FestivalManager.Application.Features.Festivals.Contracts.DTOs;
+global using Alakai.FestivalManager.Application.Features.Festivals.Services;
 global using Alakai.FestivalManager.Infrastructure.Extensions;
+global using Microsoft.AspNetCore.Mvc;
