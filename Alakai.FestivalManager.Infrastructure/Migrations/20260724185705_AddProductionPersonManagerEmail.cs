8b782ca40d71610a0e43096c12814889d9514716
Jose Maria Farfan <magoalakai@gmail.com>
2026-07-24 20:59:39 +0200

    Pending Migration

---
+++ Alakai.FestivalManager.Infrastructure/Migrations/20260724185705_AddProductionPersonManagerEmail.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Alakai.FestivalManager.Infrastructure.Migrations;
+
+/// <inheritdoc />
+public partial class AddProductionPersonManagerEmail : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<string>(
+            name: "ManagerEmail",
+            table: "ProductionPeople",
+            type: "nvarchar(max)",
+            nullable: true);
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "ManagerEmail",
+            table: "ProductionPeople");
+    }
+}
